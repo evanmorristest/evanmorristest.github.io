ad7d5a5d12e3a271ff5a8c01cda821f444a934a7
Evan Morris <evanmorris97@gmail.com>
2026-05-20 21:38:37 +0100

    reorder nav bar

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,8 +1,8 @@
 export const navigationItems = [
   { label: "Home", href: "/" },
   { label: "Projects", href: "/projects/" },
-  { label: "About", href: "/about/" },
   { label: "Blog", href: "/blog/" },
+  { label: "About", href: "/about/" },
   { label: "Contact", href: "/contact/" }
 ];
 
